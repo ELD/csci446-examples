fbdbd292cef2a685fc0d112c4bd20ce547cc9b39
Eric Dattore <eric.dattore@gmail.com>
2022-04-13 16:50:08 -0600

    add tailwind and bootstrap examples

---
+++ examples/tailwind-demo/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl bg-blue-500">Hello, TailwindCSS!</h1>
+
+      <button className="my-4 text-xl bg-slate-400 hover:bg-slate-800 hover:text-white">
+        Click Me
+      </button>
+    </>
+  );
+}
+
+export default App;
--- labs/mongodb-rest-lab/lab-frontend/src/Class.js
+++ labs/mongodb-rest-lab/lab-frontend/src/Class.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 function Class() {
