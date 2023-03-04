5c97ae47c53f050f939070dfedca7bc1105d3855
Eric Dattore <eric@dattore.me>
2023-03-03 20:45:39 -0700

    Updates to Express demo with CORS middleware

--- examples/express-demo/index.js
+++ examples/express-demo/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import blogRouter from './blog.js';
 
 const port = 3001;
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
