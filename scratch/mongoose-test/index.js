8fea2e1d1c8e5041686a3e1dfbbd9dcc9ae0807f
Eric Dattore <eric@dattore.me>
2023-04-07 16:29:53 -0600

    Update Mongoose scratch example to include embedded documents

--- scratch/mongoose-test/index.js
+++ scratch/mongoose-test/index.js
@@ -2,31 +2,50 @@ import mongoose from 'mongoose';
 
 await mongoose.connect('mongodb://localhost:27017/scratch');
 
-const Store = mongoose.model('Store', {
+// Example of embedding documents using Mongoose
+const ItemSchema = new mongoose.Schema({
   name: String,
-  products: [{
-    ref: 'Product',
-    type: mongoose.Types.ObjectId,
-  }],
+  quantity: Number
 });
 
-const Product = mongoose.model('Product', {
+const Store = mongoose.model('stores', {
+  _id: Number,
   name: String,
-  quantity: Number,
+  products: [ItemSchema],
 });
 
-const product1 = await new Product({
-  name: 'widget',
-  quantity: 10,
-}).save();
+// const store = await new Store({ _id: 1, name: 'Target', products: [] }).save();
+let store = await Store.findById(1);
+await store.updateOne({ $push: { products: { name: 'Widget', quantity: 15 }}});
+store = await Store.findById(1);
+console.log(store);
 
-const product2 = await new Product({
-  name: 'widget two',
-  quantity: 8,
-}).save();
+// Useful for related data, so one-to-many or many-to-many relations
+// const Store = mongoose.model('Store', {
+//   name: String,
+//   products: [{
+//     ref: 'Product',
+//     type: mongoose.Types.ObjectId,
+//   }],
+// });
 
-const store = await new Store({ name: 'Target', products: [product1, product2] }).save();
+// const Product = mongoose.model('Product', {
+//   name: String,
+//   quantity: Number,
+// });
 
-const stores = await Store.findOne({ _id: store._id }).populate('products');
+// const product1 = await new Product({
+//   name: 'widget',
+//   quantity: 10,
+// }).save();
 
-console.log(stores);
+// const product2 = await new Product({
+//   name: 'widget two',
+//   quantity: 8,
+// }).save();
+
+// const store = await new Store({ name: 'Target', products: [product1, product2] }).save();
+
+// const stores = await Store.findOne({ _id: store._id }).populate('products');
+
+// console.log(stores);
