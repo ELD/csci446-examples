f439c4adf8859d5bf9e2d403a12d23ee8478e5c9
Eric Dattore <eric@dattore.me>
2024-01-24 16:57:47 -0700

    Implement in-class example

---
+++ javascript-examples/examples.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("spamform");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const name = event.target[0].value;
+  const email = event.target[1].value;
+
+  alert(`Just kidding, we won't do anything with the name and email you supplied: ${name}, ${email}`);
+});
+
