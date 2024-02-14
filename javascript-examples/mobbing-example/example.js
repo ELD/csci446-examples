d3f1334aa874e4842daf9ed00173edc80601cf14
Eric Dattore <eric@dattore.me>
2024-02-14 15:40:59 -0700

    Comment and document single event listener example

--- javascript-examples/mobbing-example/example.js
+++ javascript-examples/mobbing-example/example.js
@@ -5,7 +5,8 @@ form.addEventListener("submit", (event) => {
 
   const postNumber = event.target[0].value;
 
-  // INFO: Single event listener example
+  // WARN: Single event listener example
+  // Look at line 70 for the definition of this function
   /* fetchAndPresentPost(postNumber, postRoot); */
 
   fetch(`https://dummyjson.com/posts/${postNumber}`)
@@ -67,64 +68,66 @@ function createUpdateForm(currentArticleNumber) {
   return form;
 }
 
-function fetchAndPresentPost(postNumber, rootEl, elements) {
-  if (elements === undefined) {
-    console.log('creating elements');
-    const article = document.createElement("article");
-    const header = document.createElement("h1");
-    const articleBody = document.createElement("p");
-    const tags = document.createElement("p");
-    const innerTags = document.createElement("span");
-
-    const form = document.createElement("form");
-    const postNumberInput = document.createElement("input");
-    const submitButton = document.createElement("button");
-
-    postNumberInput.type = "number";
-    postNumberInput.value = postNumber;
-
-    submitButton.type = "submit";
-    submitButton.textContent = "Replace Post";
-
-    // INFO: I forgot these in-class, which is why it didn't work
-    article.append(header);
-    article.append(articleBody);
-    article.append(tags);
-    article.append(form);
-
-    form.append(postNumberInput);
-    form.append(submitButton);
-
-    article.append(form);
-
-    elements = {
-      article,
-      header,
-      articleBody,
-      tags,
-      innerTags,
-      form,
-    };
-  }
-
-  fetch(`https://dummyjson.com/posts/${postNumber}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const { article, header, articleBody, tags, innerTags, form } = elements;
-
-      header.textContent = data.title;
-      articleBody.textContent = data.body;
-      tags.textContent = "Tags: ";
-      innerTags.textContent = data.tags.map((tag) => `#${tag}`).join(" ");
-      innerTags.className = "tags";
-      tags.append(innerTags);
-
-      form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        fetchAndPresentPost(event.target[0].value, rootEl, elements);
-      })
-
-      rootEl.append(article);
-    });
-}
+// WARN: This is not relevant unless you're going with the single event listener example
+// Check out line 10 for where this function would be called
+// function fetchAndPresentPost(postNumber, rootEl, elements) {
+//   if (elements === undefined) {
+//     console.log('creating elements');
+//     const article = document.createElement("article");
+//     const header = document.createElement("h1");
+//     const articleBody = document.createElement("p");
+//     const tags = document.createElement("p");
+//     const innerTags = document.createElement("span");
+//
+//     const form = document.createElement("form");
+//     const postNumberInput = document.createElement("input");
+//     const submitButton = document.createElement("button");
+//
+//     postNumberInput.type = "number";
+//     postNumberInput.value = postNumber;
+//
+//     submitButton.type = "submit";
+//     submitButton.textContent = "Replace Post";
+//
+//     // INFO: I forgot these in-class, which is why it didn't work
+//     article.append(header);
+//     article.append(articleBody);
+//     article.append(tags);
+//     article.append(form);
+//
+//     form.append(postNumberInput);
+//     form.append(submitButton);
+//
+//     article.append(form);
+//
+//     elements = {
+//       article,
+//       header,
+//       articleBody,
+//       tags,
+//       innerTags,
+//       form,
+//     };
+//   }
+//
+//   fetch(`https://dummyjson.com/posts/${postNumber}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const { article, header, articleBody, tags, innerTags, form } = elements;
+//
+//       header.textContent = data.title;
+//       articleBody.textContent = data.body;
+//       tags.textContent = "Tags: ";
+//       innerTags.textContent = data.tags.map((tag) => `#${tag}`).join(" ");
+//       innerTags.className = "tags";
+//       tags.append(innerTags);
+//
+//       form.addEventListener("submit", (event) => {
+//         event.preventDefault();
+//         fetchAndPresentPost(event.target[0].value, rootEl, elements);
+//       })
+//
+//       rootEl.append(article);
+//     });
+// }
 
