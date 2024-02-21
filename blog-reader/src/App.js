754d243997ff1a479e3dd7c5f9af339d453c54c6
Eric Dattore <eric@dattore.me>
2024-02-21 16:42:49 -0700

    Complete example for fetching and updating blog
    Similar to the mobbing-example/

--- blog-reader/src/App.js
+++ blog-reader/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Post from "./Post";
-import './App.css';
+import "./App.css";
+
+const BASE_URL = "https://dummyjson.com/post";
 
 function App() {
   const [postId, setPostId] = useState(null);
@@ -9,25 +11,53 @@ function App() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await fetch(`https://dummyjson.com/post/${postId}`);
+    const response = await fetch(`${BASE_URL}/${postId}`);
     if (!response.ok) {
       console.error("Failed to fetch post");
       return;
     }
     const post = await response.json();
-    setPosts([...posts, post]);
+    setPosts(posts => [...posts, post]);
   };
 
+  const updatePost = async (originalId, newId) => {
+    const response = await fetch(`${BASE_URL}/${newId}`);
+
+    if (!response.ok) {
+      console.error("Failed to fetch post");
+      return;
+    }
+    const newPost = await response.json();
+    setPosts(posts => posts.map((post) => post.id === originalId ? newPost : post))
+  };
+
+  console.log('rendered App.js');
   return (
     <div>
       <form onSubmit={(event) => handleSubmit(event)}>
-        <label htmlFor="postId">Post ID to fetch: &nbsp;
-          <input id="postId" type="number" min={0} placeholder="Post ID" onChange={(event) => setPostId(Number(event.target.value))} required />
+        <label htmlFor="postId">
+          Post ID to fetch: &nbsp;
+          <input
+            id="postId"
+            type="number"
+            min={1}
+            placeholder="Post ID"
+            onChange={(event) => setPostId(Number(event.target.value))}
+            required
+          />
         </label>
         <button type="submit">Fetch</button>
       </form>
-      <div>
-        {posts.map((post) => <Post key={post.id} title={post.title} body={post.body} />)}
+      <div className="container">
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            body={post.body}
+            updatePost={updatePost}
+          />
+        ))}
       </div>
     </div>
   );
--- blog-reader/src/Post.js
+++ blog-reader/src/Post.js
@@ -1,8 +1,24 @@
-export default function Post({ title, body }) {
+import { useRef } from "react";
+
+export default function Post(props) {
+  const { id, title, body, updatePost } = props;
+  const inputRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updatePost(id, Number(inputRef.current.value));
+  }
+
+  console.log('rendered Post.js');
   return (
     <article>
       <h1>{title}</h1>
       <p>{body}</p>
+      <form onSubmit={handleSubmit}>
+        <label>Post ID:</label>
+        <input type="number" defaultValue={id} min={1} ref={inputRef} />
+        <button type="submit">Update Post</button>
+      </form>
     </article>
   );
 }
